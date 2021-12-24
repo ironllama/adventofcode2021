3d28d1203b1cf7c9f5cc310bbc3371f83356e9e3
Alex <alexoh@wcoding.com>
2021-12-24 17:03:55 +0900

    19 still not right

--- 19a.rs
+++ 19a.rs
@@ -1,6 +1,7 @@
 pub mod usrlib;
 
 use std::collections::VecDeque;
+use std::collections::HashSet;
 
 fn main() {
     let input_stuff = [
@@ -185,7 +186,7 @@ fn main() {
     let mut input_stuff_vec: Vec<Vec<(i32, i32, i32)>> = vec![];
     let mut scanner_vec = vec![];
     for idx in 0..input_stuff.len() {
-        if input_stuff[idx].is_empty() {
+        if input_stuff[idx].is_empty() {  // Blank lines between scanners.
             input_stuff_vec.push(scanner_vec);
             scanner_vec = vec![];
         }
@@ -198,38 +199,45 @@ fn main() {
     input_stuff_vec.push(scanner_vec);  // Don't forget the last set!
     // println!("INPUT: {:?}", input_stuff_vec);
 
-    fn compare_beacons(one_vec: &Vec<(i32, i32, i32)>, two_vec: &mut VecDeque<(i32, i32, i32)>) -> Vec<(i32, i32, i32)> {
+    fn compare_beacons(one_vec: &Vec<(i32, i32, i32)>, two_vec: &mut VecDeque<(i32, i32, i32)>, two_vec_offset: &mut (i32, i32, i32)) -> Vec<(i32, i32, i32)> {
         let mut scanner_matches: Vec<(i32, i32, i32)> = vec![];
 
-        // Start comparing the two scanners' beacon lists.
-        for _uno in 0..two_vec.len() {  // Try all the beacons as the "origin" beacon.
-            let uno_vertex: (i32, i32, i32) = one_vec[0];
-            scanner_matches = vec![];
-            let mut offsets = (0, 0, 0);
+        'outer: for uno in 0..one_vec.len() {
+            let uno_vertex: (i32, i32, i32) = one_vec[uno];  // Next "origin" beacon. Since they can be spread out anywhere.
+            // let mut offset = (0, 0, 0);
 
-            let dos_vertex: (i32, i32, i32) = two_vec[0];
-            offsets = (dos_vertex.0 - uno_vertex.0, dos_vertex.1 - uno_vertex.1, dos_vertex.2 - uno_vertex.2);
-            println!("\tOFFSET: {:?}", offsets);
-            // let offset_vertex: VecDeque<(i32, i32, i32)> = two_vec.clone();
+            // Start comparing the two scanners' beacon lists.
+            for _dos in 0..two_vec.len() {  // Try lining each beacon from the second list with the current "origin" beacon.
+                let dos_vertex: (i32, i32, i32) = two_vec[0];
+                scanner_matches = vec![];
+                let offset = (dos_vertex.0 - uno_vertex.0, dos_vertex.1 - uno_vertex.1, dos_vertex.2 - uno_vertex.2);
+                // println!("\tOFFSET: {:?}", offset);
+                // let offset_vertex: VecDeque<(i32, i32, i32)> = two_vec.clone();
 
-            two_vec.iter().for_each(|xyz| {
-                let new_xyz = (xyz.0 - offsets.0, xyz.1 - offsets.1, xyz.2 - offsets.2);
-                // if offsets == (-68, 1246, 43) {
-                //     println!("\t\tCOMP: {:?} - {:?} = {:?}", xyz, offsets, new_xyz);
-                // }
-               if one_vec.contains(&new_xyz) {
-                    // println!("MATCHES! {:?}", new_xyz);
-                    scanner_matches.push(new_xyz);
+                two_vec.iter().for_each(|xyz| {
+                    let new_xyz = (xyz.0 - offset.0, xyz.1 - offset.1, xyz.2 - offset.2);
+                    // if offset == (-68, 1246, 43) {
+                    //     println!("\t\tCOMP: {:?} - {:?} = {:?}", xyz, offset, new_xyz);
+                    // }
+                    if one_vec.contains(&new_xyz) {  // Since the lists are not in any order, even relative to each other.
+                        // println!("MATCHES! {:?}", new_xyz);
+                        scanner_matches.push(new_xyz);
+                    }
+                });
+
+                // if scanner_matches.len() >= 6 { // Found a pair and orientation that matches!);
+                if scanner_matches.len() >= 12 { // Found a pair and orientation that matches!);
+                    println!("COMPARE MATCHED!");
+                    *two_vec_offset = offset;
+
+                    // Apply the offset to all the results.
+                    two_vec.iter_mut().for_each(|xyz| *xyz = (xyz.0 - offset.0, xyz.1 - offset.1, xyz.2 - offset.2));
+
+                    break 'outer;
                 }
-            });
 
-            // if scanner_matches.len() >= 6 { // Found a pair and orientation that matches!);
-            if scanner_matches.len() >= 12 { // Found a pair and orientation that matches!);
-                println!("COMPARE MATCHED!");
-                break;
+                two_vec.rotate_left(1);  // Move vertices over by 1 -- try aligning a  different point with "origin".
             }
-
-            two_vec.rotate_left(1);  // Move vertices over by 1 -- try a different point as "origin".
         }
 
 
@@ -343,7 +351,8 @@ fn main() {
 
 
     // let mut matches: Vec<Vec<(i32, i32, i32)>> = vec![];
-    let mut new_beacons: i32 = 0;
+    let mut new_beacons: HashSet<(i32, i32, i32)> = HashSet::new();
+    let mut scanner_offsets: Vec<(i32, i32, i32)> = vec![(0, 0, 0); input_stuff_vec.len()];
     // let one = 0;
     for one in 0..(input_stuff_vec.len() - 1) {
         println!("SOURCE SCANNER: {}", one);
@@ -352,6 +361,8 @@ fn main() {
             // input_stuff_vec[one].sort();
             let one_vec = &input_stuff_vec[one];
             let mut two_vec: VecDeque<(i32, i32, i32)> = VecDeque::from(input_stuff_vec[two].clone());
+
+            // let one_vec_offset = scanner_offsets[one];
 
             // if one_vec.len() != two_vec.len() {
             //     panic!("PROBLEMS with LENGTHS {:?} {:?}", one_vec, two_vec);
@@ -368,15 +379,35 @@ fn main() {
                 // println!("TWO_VEC:");
                 // for vertex in &two_vec { println!("{:?}", vertex); }
 
-                let mut new_matches: Vec<(i32, i32, i32)> = compare_beacons(&one_vec, &mut two_vec);
+                let new_matches: Vec<(i32, i32, i32)> = compare_beacons(&one_vec, &mut two_vec, &mut scanner_offsets[two]);
                 // if new_matches.len() >= 6 {
                 if new_matches.len() >= 12 {
-                    println!("MATCHES! SCANNER {} AND {}", one, two);
                     // new_matches.sort();
+                    // Display.
                     new_matches.iter().for_each(|x| println!("{:?}", x));
                     println!();
 
-                    new_beacons += (two_vec.len() - new_matches.len()) as i32;
+                    // new_beacons += ((one_vec.len() + two_vec.len()) - new_matches.len()) as i32;
+                    // Run everything relative to beacon 0
+                    let scan_location = ((scanner_offsets[two].0 + scanner_offsets[one].0) * -1, (scanner_offsets[two].1 + scanner_offsets[one].1) * -1, (scanner_offsets[two].2 + scanner_offsets[one].2) * -1);
+                    new_matches.iter().for_each(|x| {
+
+
+
+                        // TODO: Instead of inserting matching beacons, filter them out from the union of the two vectors.
+                        new_beacons.insert((x.0 + scanner_offsets[one].0, x.1 + scanner_offsets[one].1, x.2 + scanner_offsets[one].2));
+
+
+
+                    });
+
+                    println!("MATCHES! SCANNER {} AND {} at {:?} LEN {} NUM_BEACONS {}",
+                        one,
+                        two,
+                        scan_location,
+                        new_matches.len(),
+                        new_beacons.len()
+                    );
                     break;
                 }
             }
@@ -390,5 +421,7 @@ fn main() {
         }
     }
 
-    println!("NUM {}", new_beacons);
+    println!("BEACONS {:?}", new_beacons.iter().for_each(|x| println!("{:?}", x)));
+    println!("OFFSETS {:?}", scanner_offsets.iter().for_each(|x| println!("{:?}", x)));
+    println!("NUM {}", new_beacons.len());
 }
